5f8fb73ff157ce1871fff47b45b348218f2ef126
JasonWang <wlh2119@gmail.com>
2017-12-16 22:28:00 +0800

    update glide to 4.1.1

--- app/proguard-rules.pro
+++ app/proguard-rules.pro
@@ -20,4 +20,26 @@
 -keep public class * implements com.bumptech.glide.module.GlideModule
 -keepnames class com.github.runningforlife.photofans.glide.DiskStorageModule
 # for DexGuard only
-#-keepresourcexmlelements manifest/application/meta-data@value=GlideModule
+-keepresourcexmlelements manifest/application/meta-data@value=GlideModule
+# Glide
+-keep public class * implements com.bumptech.glide.module.GlideModule
+-keep public enum com.bumptech.glide.load.resource.bitmap.ImageHeaderParser$** {
+    **[] $VALUES;
+    public *;
+}
+# nineoldandroids
+-keep interface com.nineoldandroids.view.** { *; }
+-dontwarn com.nineoldandroids.**
+-keep class com.nineoldandroids.** { *; }
+# support-v7-appcompat
+-keep public class android.support.v7.widget.** { *; }
+-keep public class android.support.v7.internal.widget.** { *; }
+-keep public class android.support.v7.internal.view.menu.** { *; }
+-keep public class * extends android.support.v4.view.ActionProvider {
+    public <init>(android.content.Context);
+}
+# support-design
+-dontwarn android.support.design.**
+-keep class android.support.design.** { *; }
+-keep interface android.support.design.** { *; }
+-keep public class android.support.design.R$* { *; }
--- app/src/main/java/com/github/runningforlife/photosniffer/glide/DiskStorageModule.java
+++ app/src/main/java/com/github/runningforlife/photosniffer/glide/DiskStorageModule.java
@@ -1,37 +1,23 @@
 package com.github.runningforlife.photosniffer.glide;
 
 import android.content.Context;
-import android.graphics.BitmapFactory;
-import android.util.DisplayMetrics;
 import android.util.Log;
 
-import com.bumptech.glide.Glide;
 import com.bumptech.glide.GlideBuilder;
-import com.bumptech.glide.load.DecodeFormat;
-import com.bumptech.glide.module.GlideModule;
-import com.github.runningforlife.photosniffer.utils.DisplayUtil;
+import com.bumptech.glide.module.AppGlideModule;
 
 /**
  *  a module to setup our own glide params
  */
 
-public class DiskStorageModule implements GlideModule {
+@com.bumptech.glide.annotation.GlideModule
+public class DiskStorageModule extends AppGlideModule {
     private static final String TAG = "DiskStorageModule";
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
         Log.d(TAG,"applyOptions()");
 
-        if(DisplayUtil.isDeviceHighPerf()) {
-            builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888)
-                    .setDiskCache(new DiskLruFactory(context));
-        }else{
-            builder.setDecodeFormat(DecodeFormat.PREFER_RGB_565)
-                    .setDiskCache(new DiskLruFactory(context));
-        }
+        builder.setDiskCache(new DiskLruFactory(context));
     }
 
-    @Override
-    public void registerComponents(Context context, Glide glide) {
-        // do nothing
-    }
 }
