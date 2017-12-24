3fe773ea9c1fa8d3b1f7afb39f1c06e8bc5d958f
JasonWang <wlh2119@gmail.com>
2017-12-24 21:56:03 +0800

    bugFix: fail to wallpaper setting

--- app/src/main/java/com/github/runningforlife/photosniffer/utils/WallpaperUtils.java
+++ app/src/main/java/com/github/runningforlife/photosniffer/utils/WallpaperUtils.java
@@ -161,8 +161,7 @@ public class WallpaperUtils {
         RealmApi realmApi = RealmApiImpl.getInstance();
         HashMap<String, String> params = new HashMap<>();
         params.put("mIsWallpaper", Boolean.toString(Boolean.TRUE));
-        RealmResults<ImageRealm> wallpapers = (RealmResults<ImageRealm>) realmApi.queryAsync(ImageRealm.class, params);
-
+        RealmResults<ImageRealm> wallpapers = (RealmResults<ImageRealm>) realmApi.querySync(ImageRealm.class, params);
 
         if (wallpapers.size() == 0) {
             // try to cache images
