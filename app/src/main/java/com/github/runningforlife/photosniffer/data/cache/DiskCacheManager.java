3e51ac100d2ff07633c1362a5a48318cc82599ac
JasonWang <wlh2119@gmail.com>
2017-12-28 15:54:57 +0800

    bitmap data is not cached correctly

--- app/src/main/java/com/github/runningforlife/photosniffer/data/cache/DiskCacheManager.java
+++ app/src/main/java/com/github/runningforlife/photosniffer/data/cache/DiskCacheManager.java
@@ -119,6 +119,7 @@ public class DiskCacheManager implements CacheApi {
                 case ACTION_PUT:
                     if (bitmap != null) {
                         ByteArrayOutputStream bos = new ByteArrayOutputStream(bitmap.getByteCount());
+                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bos);
                         Cache.Entry entry2 = new Cache.Entry(bos.toByteArray(), System.currentTimeMillis());
                         mDiskCache.put(url, entry2);
                     }
